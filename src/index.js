b72cb8901f28f30e1c82a4eebe3b499e782628e8
Vitaliy Timchenko [home] <vitaliy.timchenko@gmail.com>
2020-02-28 00:00:05 +0200

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import './style.css';
+import printMe from './print.js';
 
-function component () {
+function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
+
+    btn.innerHTML = 'Click me and check the console...';
+    btn.onclick = printMe;
+
     element.innerHTML = '<strong>Dynamically</strong> generated content...';
     element.classList.add('title');
+    element.appendChild(btn);
 
     return element;
 }
